b829443c7d916321934dcefc8a1eeb95b6eafb8a
Avinash <avinash.d@knabfinance.com>
2020-01-26 00:09:08 +0530

    removed print statement

--- ecommerce-problem.php
+++ ecommerce-problem.php
@@ -12,19 +12,19 @@ function getTotalPrice($items){
    $total_items = count($items);
    $item_count = array_count_values($items);
    $unique_items = count($item_count);
-   var_dump($item_count);
+   //var_dump($item_count);
    $total_price = 0;
    foreach($item_count as $item => $value){
 
      $pricing_rules = $product_catalog['pricingRules'][$item]; 
      if($pricing_rules['IsBuyXGetYFree']){
-     	$total_price += getBuyXGetYPrice($pricing_rules,$item,$value);
+      $total_price += getBuyXGetYPrice($pricing_rules,$item,$value);
      }
      else if($pricing_rules['IsBulkQtyOffer']){
-     	$total_price += getBulkItemsPrice($pricing_rules,$item,$value);
+      $total_price += getBulkItemsPrice($pricing_rules,$item,$value);
      }
      else{
-     	$total_price += $value * $pricing_rules['Price'];
+      $total_price += $value * $pricing_rules['Price'];
      }
    }
    return $total_price;
@@ -35,7 +35,7 @@ function getBuyXGetYPrice($pricing_rules,$item,$item_count){
    $items_reminder = $item_count % $offer_quantity ;
    $items_actual = intdiv($item_count, $offer_quantity); 
    $items_total_price = ($items_actual + $items_reminder) * $pricing_rules['Price']; 
-   echo $items_total_price;
+   //echo $items_total_price;
   return $items_total_price;
 }
 function getBulkItemsPrice($pricing_rules,$item,$item_count){
@@ -43,20 +43,20 @@ function getBulkItemsPrice($pricing_rules,$item,$item_count){
    $price_reduction = (isset($pricing_rules['bulkQtyPriceReduction']))
    ? : 0;
    if($item_count >= $bulk_qty_lower_limit){
-   	     $items_total_price = $item_count * ($pricing_rules['Price'] - $price_reduction);
+         $items_total_price = $item_count * ($pricing_rules['Price'] - $price_reduction);
    }
    else{
-   	    $items_total_price = $item_count * $pricing_rules['Price']; 
+        $items_total_price = $item_count * $pricing_rules['Price']; 
    }
    return $items_total_price;
 }
 
 
 $buy_items = trim(fgets(STDIN));
-var_dump($buy_items);
+//var_dump($buy_items);
 $buy_list = explode(" ",$buy_items);
-var_dump($buy_list);
+//var_dump($buy_list);
 $final_price = getTotalPrice($buy_list);
 
-echo "Total payable price of cart is $final_price";
+echo "Total payable price of cart is $final_price\n";
 ?>
